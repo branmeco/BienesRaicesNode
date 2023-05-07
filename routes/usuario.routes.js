4cfcd71744ad6b4f4a15da4397258960bdee29a4
branmeco <branmeco17@gmail.com>
2023-05-06 22:30:50 -0500

    Probando Peticiones GET y POST

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -3,11 +3,12 @@ import express from "express";
 const router = express.Router();
 
 //Routing
-app.get('/', function (req, res) {
-    res.send('Hola mundo en express')
+router.get('/', (req, res) => {
+    res.send('Hola mundo en express');
 });
-app.get('/nosotros', function (req, res) {
-    res.send('Información de nosotros')
+
+router.post('/', (req, res) => {
+    res.json({msg: 'Información de nosotros'});
 });
 
 export default router;
