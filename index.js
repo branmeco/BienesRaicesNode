4720f6fde1b9becadd86e07cd1400a414b561b96
branmeco <branmeco17@gmail.com>
2023-05-06 21:56:59 -0500

    Creando un servidor con express

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+//Crear la app
+const app = express();
+
+//Routing
+app.get('/', function(req, res){
+    res.send('Hola mundo en express')
+});
+app.get('/nosotros', function(req, res){
+    res.send('Información de nosotros')
+});
+
+//Definir un puerto y arrancar el proyecto
+const port = 4000;
+app.listen(port, () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
+})
