7b6ba1ed21d2f1394f97d2458142062572bb6ea9
branmeco <branmeco17@gmail.com>
2023-05-07 06:21:13 -0500

    Añadiendo más rutas

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { formularioLogin } from "../controllers/usuario.controller.js";
+import { formularioLogin, formularioRegistro } from "../controllers/usuario.controller.js";
 
 const router = express.Router();
 
 //Routing
 router.get('/login', formularioLogin);
-
+router.get('/registro', formularioRegistro);
 
 export default router;
